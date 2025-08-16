d2933e5820713a4e316c31240d49ba676a761850
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-16 16:17:58 -0500

    Add the initial array, object, and constructor

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("connected");
+const myLibrary = [];
+
+function Book(author, title, numPages, format, readStatus, id) {
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.format = format;
+  this.readStatus = readStatus;
+  this.id = id;
+}
+
+function addBookToLibrary(author, title, numPages, format, readStatus) {
+  const id = window.crypto.randomUUID();
+  const book = new Book(author, title, numPages, format, readStatus, id);
+  myLibrary.push(book);
+}
+
+// function displayLibrary(array) {
+//   const output = document.querySelector(".lib-output");
+
+//   for (let book in array) {
+//     // create html element
+//     // then, add to output
+//   }
+// }
+
+// function for looping through array
+// function for displaying array
+
+// add new book button on library
+// tie "new book" button to form (author, title, # pages, unread/read)
+// preventDefault will come in handy
+
+// remove book button on book
+
+// unread/read toggle on book
